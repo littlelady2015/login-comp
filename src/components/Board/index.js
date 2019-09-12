2a028e56c6cf59f7762dd1957c331743fc83a876
zhangyurui <zhangyurui@beisen.com>
2019-09-12 17:02:17 +0800

    feat(board)

---
+++ src/components/Board/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+function Board() {
+  return <div className="board-wrapper">
+           <div className="main_img_container"></div>
+         </div>
+}
+export default Board;
